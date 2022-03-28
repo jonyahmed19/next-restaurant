bbde7d133364b4d3c0f6c352341561cf457ba939
Sahidul Islam <jonyahmed19@gmail.com>
2022-03-28 09:49:17 +0600

    Initial setup is done

---
+++ components/Layout.jsx
@@ -0,0 +1,14 @@
+import Footer from "./Footer";
+import Navbar from "./Navbar";
+
+const Layout = (props) => {
+  return (
+    <>
+      <Navbar />
+      {props.children}
+      <Footer />
+    </>
+  );
+};
+
+export default Layout;
