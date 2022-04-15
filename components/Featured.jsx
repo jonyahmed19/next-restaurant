64ab4a84cfd38ab8072b35b473148726186db35b
Sahidul Islam <jonyahmed19@gmail.com>
2022-04-15 08:44:17 +0600

    Added code

--- components/Featured.jsx
+++ components/Featured.jsx
@@ -1,15 +1,28 @@
 import styles from "../styles/Featured.module.css";
 import Image from "next/image";
+
+const images = [
+  "/img/featured1.jpg",
+  "/img/featured2.jpg",
+  "/img/featured3.jpg",
+];
+
 const Featured = () => {
   return (
     <div className={styles.container}>
-      <Image src="/img/arrowl.png" width="100px" height="100px" alt="" />
-      <div className={styles.wrapper}>
-        <div className={styles.imgContainer}>
-          <Image src="/img/arrowr.png" width="100px" height="100px" alt="" />
-        </div>
+      <div className={styles.arrowContainer} style={{ left: 0 }}>
+        <Image src="/img/arrowl.png" layout="fill" alt="" />
       </div>
-      <Image src="/img/arrowr.png" width="100px" height="100px" alt="" />
+      <div className={styles.wrapper}>
+        {images.map((img, index) => (
+          <div key={index} className={styles.imgContainer}>
+            <Image src={img} layout="fill" alt="" objectFit="contain" />
+          </div>
+        ))}
+      </div>
+      <div className={styles.arrowContainer} style={{ right: 0 }}>
+        <Image src="/img/arrowr.png" layout="fill" alt="" />
+      </div>
     </div>
   );
 };
